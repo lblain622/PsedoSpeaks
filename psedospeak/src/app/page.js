76119a4bf80b1aa0c09efd244ba9fe6caa7792dc
Vanessa <vn4261@rit.edu>
2025-03-01 18:17:15 -0500

    Adding stuff

--- psedospeak/src/app/page.js
+++ psedospeak/src/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import ToggleInvertColors from './theme';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <ToggleInvertColors />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -26,7 +28,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="/frontui"
             target="_blank"
             rel="noopener noreferrer"
           >
