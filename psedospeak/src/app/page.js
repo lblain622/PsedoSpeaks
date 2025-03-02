73b9e6db266edba917b121c673f7179813fe022f
Vanessa <vn4261@rit.edu>
2025-03-01 23:28:56 -0500

    Adding button for changing theme

--- psedospeak/src/app/page.js
+++ psedospeak/src/app/page.js
@@ -1,9 +1,15 @@
 import AudioRecorder from "@/components/AudioRecorder";
+import ToggleInvertColors from "@/components/theme";
 
 export default function Home() {
   return (
     <div>
-      <AudioRecorder />
+      <div style={{ position: "fixed", top: "10px", right: "10px", zIndex: 1000 }}>
+        <ToggleInvertColors />
+      </div>
+      <div className="content">
+        <AudioRecorder />
+      </div>
     </div>
   );
 }
